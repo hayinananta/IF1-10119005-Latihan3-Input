de017134d5cb0d35e952968b233dd58d98aee7b3
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 10:42:00 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan3/PBO110119005Latihan3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Memasukkan nilai dari keyboard
+ */
+public class PBO110119005Latihan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+    }
+    
+}
